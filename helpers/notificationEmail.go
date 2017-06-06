2a242afb1a16736bb07ff1a96e4e215bc428e82e
Matt Arnold <matt@mattarnster.co.uk>
2017-06-06 18:17:51 +0100

    Filling in the new template

--- helpers/notificationEmail.go
+++ helpers/notificationEmail.go
@@ -1,6 +1,8 @@
 package helpers
 
 import (
+	"bytes"
+	"html/template"
 	"log"
 	"os"
 
@@ -8,6 +10,12 @@ import (
 
 	mailgun "gopkg.in/mailgun/mailgun-go.v1"
 )
+
+type templateVars struct {
+	RepoName    string
+	ReleaseBody string
+	ReleaseTag  string
+}
 
 // SendNotificationEmail does exactly what it says on the tin.
 func SendNotificationEmail(repo models.Repo, email string, release models.Release) {
@@ -18,11 +26,27 @@ func SendNotificationEmail(repo models.Repo, email string, release models.Releas
 		"Hey there, A new release for "+repo.Repo+" has been detected!",
 		email)
 
-	message.SetHtml("<h1>releasetrackr</h1><p>Hey there.</p><p>A new release for " + repo.Repo + " has been detected!</p><p>Tag: " + release.Tag + "</p><p>" + release.Body + "</p>")
+	t, _ := template.ParseFiles("templates/new-release.html")
+	var doc bytes.Buffer
+	err := t.Execute(&doc, generateTemplateVars(repo, release))
+
+	if err != nil {
+		log.Printf("Template parse failed: %v", err.Error())
+	}
+
+	message.SetHtml(doc.String())
 
 	resp, id, err := mg.Send(message)
 	if err != nil {
 		log.Fatal(err)
 	}
 	log.Printf("[Helper][NotificationEmail][Mailgun] Queued ID: %s Resp: %s\n", id, resp)
+}
+
+func generateTemplateVars(repo models.Repo, release models.Release) templateVars {
+	return templateVars{
+		RepoName:    repo.Repo,
+		ReleaseBody: release.Body,
+		ReleaseTag:  release.Tag,
+	}
 }
